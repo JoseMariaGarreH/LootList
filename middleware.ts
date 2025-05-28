c32ae8cbf4b8a77a76f0e27232ac29180736ffdc
JoseMariaGarreH <josemariagarrehermosilla5@gmail.com>
2025-05-28 02:02:37 +0200

    Configuración del middleware con nuevos datos

--- middleware.ts
+++ middleware.ts
@@ -18,18 +18,32 @@ export async function middleware(request: NextRequest) {
         /^\/[^\/]+$/.test(pathname) &&
         !publicPaths.some(route => pathname.startsWith(route));
 
-    const isUserLibraryRoute =
-        /^\/[^\/]+\/library$/.test(pathname);
+    const isUserWishlistRoute = /^\/[^\/]+\/wishlist$/.test(pathname);
+    const isUserLikesRoute = /^\/[^\/]+\/likes$/.test(pathname);
+    const isUserPlayingRoute = /^\/[^\/]+\/playing$/.test(pathname);
+    const isUserPlayedRoute = /^\/[^\/]+\/played$/.test(pathname);
 
-    if ((pathname.startsWith("/settings") || isProfileRoute || isUserLibraryRoute) && !token) {
+    if (
+        (
+            pathname.startsWith("/settings") ||
+            isProfileRoute ||
+            isUserPlayedRoute ||
+            isUserWishlistRoute ||
+            isUserLikesRoute ||
+            isUserPlayingRoute
+        ) && !token
+    ) {
         return NextResponse.redirect(new URL("/auth/login", request.url));
     }
 
     if (
         token &&
-        (pathname.startsWith("/auth/login") ||
+        (   
+            pathname.startsWith("/auth/login") ||
             pathname.startsWith("/auth/signup") ||
-            pathname.startsWith("/auth/resetpassword"))
+            pathname.startsWith("/auth/resetpassword") ||
+            pathname.startsWith("/auth/requestemail")
+        )
     ) {
         return NextResponse.redirect(new URL("/", request.url));
     }
